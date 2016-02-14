6347e1a40775d7fec310d4e9ad9c52d5c614f8dc
Tomasz Maćkowiak <kurazu@kurazu.net>
2016-02-14 12:48:47 +0100

    sync

--- examples/sync3.js
+++ examples/sync3.js
@@ -1,23 +1,21 @@
-function countToMilion() {
-    for (i =0; i < Math.pow(10, 8); i++) { /* do nothing */ }
-}
-
 console.log('before');
 setTimeout(console.log.bind(console, 'timeout before'), 0);
 var promise = new Promise(function(resolve, reject) {
     setTimeout(console.log.bind(console, 'timeout A'), 0);
-    countToMilion();
     console.log('A');
     resolve('A');
-    countToMilion();
 }).then(function(input) {
     setTimeout(console.log.bind(console, 'timeout B'), 0);
-    countToMilion();
     console.log('B');
+    new Promise(function(resolve, reject) {
+        console.log('I');
+        resolve('I');
+    }).then(function(i) {
+        console.log('I handler', i);
+    });
     return input + 'B';
 }).then(function(input) {
     setTimeout(console.log.bind(console, 'timeout C'), 0);
-    countToMilion();
     console.log('C');
     return input + 'C';
 });
