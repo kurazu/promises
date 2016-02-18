a7a7e54a51e29d6ca0a6d287e7c3691f379693de
Tomasz Maćkowiak <kurazu@kurazu.net>
2016-02-18 18:32:46 +0100

    combine

---
+++ examples/chain_orig.js
@@ -0,0 +1,30 @@
+function fetchCities() {
+    var cities = ['Poznań', 'Wrocław', 'Piła', 'Łódź'];
+    return new Promise(function(resolve, reject) {
+        setTimeout(resolve.bind(null, cities), 1000);
+    });
+}
+
+function upperCase(array) {
+    return array.map(function(elem) {
+        return elem.toUpperCase();
+    });
+}
+
+function pad(array) {
+    var maxLength = 0;
+    array.forEach(function(elem) {
+        maxLength = Math.max(maxLength, elem.length);
+    });
+    return array.map(function(elem) {
+        return elem + '*'.repeat(maxLength - elem.length);
+    });
+}
+
+var promise = fetchCities().then(upperCase).then(pad);
+promise.then(function(result) {
+    console.log(result);
+});
+promise.catch(function(err) {
+    console.log(err);
+});
